3e3bedae3a664c1cfa0730dd61a7e9a799160c94
Julius Cavalcante <julius.c@outlook.com.br>
2022-09-13 19:21:14 -0300

    account repository layer mapping

--- thisfinans/src/main/java/com/programeiros/thisfinans/repositories/AccountRespository.java
+++ thisfinans/src/main/java/com/programeiros/thisfinans/repositories/AccountRespository.java
@@ -1,4 +1,10 @@
 package com.programeiros.thisfinans.repositories;
 
-public interface AccountRespository {
+import com.programeiros.thisfinans.model.entities.Account;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AccountRespository extends JpaRepository<Account, Long> {
+
 }
