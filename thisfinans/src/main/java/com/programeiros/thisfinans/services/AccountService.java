d38e408a3a122f792b003aa5ac1d27c2587792c3
Julius Cavalcante <julius.c@outlook.com.br>
2022-09-13 19:22:27 -0300

    account service layer mapping

--- thisfinans/src/main/java/com/programeiros/thisfinans/services/AccountService.java
+++ thisfinans/src/main/java/com/programeiros/thisfinans/services/AccountService.java
@@ -1,4 +1,21 @@
 package com.programeiros.thisfinans.services;
 
-public class AccountService {
+import com.programeiros.thisfinans.model.entities.Account;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public interface AccountService {
+
+    Account insert(Account account);
+
+    Account update(Account account);
+
+    Optional<Account> findById(Long id);
+
+    void delete(Long id);
+
+    List<Account> findAll();
 }
