b94082e74f03db059c24a4f66e3f619be5418b57
mmacedoaraujo <marcomacedoaraujo@gmail.com>
2022-09-14 11:34:58 -0300

    cleaned project

--- thisfinans/src/main/java/com/programeiros/thisfinans/repositories/UserConfigRepository.java
+++
@@ -1,7 +0,0 @@
-package com.programeiros.thisfinans.repositories;
-
-import com.programeiros.thisfinans.model.entities.UserConfig;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserConfigRepository extends JpaRepository<UserConfig, Long> {
-}
--- thisfinans/src/main/java/com/programeiros/thisfinans/repositories/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.programeiros.thisfinans.repositories;
-
-import com.programeiros.thisfinans.model.entities.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-
-    boolean existsUserByEmail(String email);
-}
