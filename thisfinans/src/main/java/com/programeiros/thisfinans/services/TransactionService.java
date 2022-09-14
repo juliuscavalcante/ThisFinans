574075681feb96fb3f194f35b99495c2d4e962b3
mmacedoaraujo <marcomacedoaraujo@gmail.com>
2022-09-14 13:52:25 -0300

    implemented and tested: delete() and insert() service methods, created exceptions: DatabaseException, ResourceNotFoundException

--- thisfinans/src/main/java/com/programeiros/thisfinans/services/TransactionService.java
+++ thisfinans/src/main/java/com/programeiros/thisfinans/services/TransactionService.java
@@ -1,11 +1,15 @@
 package com.programeiros.thisfinans.services;
 
+import com.programeiros.thisfinans.DB.DBException;
 import com.programeiros.thisfinans.model.dto.TransactionDTO;
 import com.programeiros.thisfinans.model.dto.TransactionEntryDTO;
 import com.programeiros.thisfinans.model.entities.Transaction;
 import com.programeiros.thisfinans.repositories.TransactionRepository;
+import com.programeiros.thisfinans.services.exceptions.ResourceNotFoundException;
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -26,6 +30,22 @@ public class TransactionService {
     public TransactionDTO findById(Long id) {
         Optional<Transaction> transaction = repository.findById(id);
 
-        return new TransactionDTO(transaction.get());
+        return new TransactionDTO(transaction.orElseThrow(() -> new ResourceNotFoundException(id)));
+    }
+
+    public void delete(Long id) {
+        try {
+            if(repository.findById(id).isPresent()) {
+                repository.delete(repository.findById(id).get());
+            }
+        } catch (EmptyResultDataAccessException ex) {
+            throw new ResourceNotFoundException(id);
+        } catch (DataIntegrityViolationException ex) {
+            throw new DBException(ex.getMessage());
+        }
+    }
+    
+    public void insert(Transaction transaction) {
+        repository.save(transaction);
     }
 }
--- thisfinans/src/test/java/com/programeiros/thisfinans/services/TransactionServiceTest.java
+++ thisfinans/src/test/java/com/programeiros/thisfinans/services/TransactionServiceTest.java
@@ -9,13 +9,11 @@ import org.aspectj.lang.annotation.Before;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.ArgumentCaptor;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
+import org.mockito.*;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.mockito.stubbing.OngoingStubbing;
 
+import javax.swing.text.html.parser.Entity;
 import java.math.BigDecimal;
 import java.time.Instant;
 import java.util.*;
@@ -34,14 +32,10 @@ class TransactionServiceTest {
     @InjectMocks
     TransactionService underTest;
 
-
-    @BeforeEach
-    void setUp() {
-        underTest = new TransactionService(transactionRepository);
-    }
+    Transaction transaction = new Transaction(22L,UUID.randomUUID(),"Teste",TransactionType.EXPENSES,TransactionStatus.COMPLETE,BigDecimal.valueOf(20),Boolean.FALSE,Instant.now(),Instant.now(),Instant.now(),null);
 
     @Test
-    void canFindAll() {
+    void itShouldFindAll() {
         //when
         underTest.findAll();
         //then
@@ -49,8 +43,8 @@ class TransactionServiceTest {
     }
 
     @Test
-    void canItFindById() {
-        Transaction transaction = new Transaction(22L,UUID.randomUUID(),"Teste",TransactionType.EXPENSES,TransactionStatus.COMPLETE,BigDecimal.valueOf(20),Boolean.FALSE,Instant.now(),Instant.now(),Instant.now(),null);
+    void itShouldFindById() {
+
 
        when(transactionRepository.findById(1L)).thenReturn(Optional.of(transaction));
        TransactionDTO testTransaction = underTest.findById(1L);
@@ -66,17 +60,40 @@ class TransactionServiceTest {
        assertEquals(transaction.getUpdateDate(), testTransaction.getUpdateDate());
        assertEquals(transaction.getAccountTransactions(), testTransaction.getAccountTransactions());
 
+    }
 
-//        this.id = entity.getId();
-//        this.cod = entity.getCod();
-//        this.description = entity.getDescription();
-//        this.type = entity.getType();
-//        this.amount = entity.getAmount();
-//        this.deleted = entity.getDeleted();
-//        this.transactionDate = entity.getTransactionDate();
-//        this.createDate = entity.getCreateDate();
-//        this.updateDate = entity.getUpdateDate();
-//        this.accountTransactions = entity.getAccountTransactions();
+    @Test
+    void itShouldDelete() {
+        when(transactionRepository.findById(1L)).thenReturn(Optional.of(transaction));
+
+        underTest.delete(1L);
+
+        verify(transactionRepository, times(1)).delete(transaction);
+    }
+
+    @Test
+    void itShouldInsert() {
+        //when
+        underTest.insert(transaction);
+
+        //then
+        ArgumentCaptor<Transaction> transactionArgumentCaptor = ArgumentCaptor.forClass(Transaction.class);
+
+        verify(transactionRepository).save(transactionArgumentCaptor.capture());
+
+        TransactionDTO capturedTransaction = new TransactionDTO(transactionArgumentCaptor.getValue());
+
+        assertEquals(capturedTransaction.getId(), transaction.getId());
+        assertEquals(capturedTransaction.getCod(), transaction.getCod());
+        assertEquals(capturedTransaction.getDescription(), transaction.getDescription());
+        assertEquals(capturedTransaction.getType(), transaction.getType());
+        assertEquals(capturedTransaction.getAmount(), transaction.getAmount());
+        assertEquals(capturedTransaction.getDeleted(), transaction.getDeleted());
+        assertEquals(capturedTransaction.getTransactionDate(), transaction.getTransactionDate());
+        assertEquals(capturedTransaction.getCreateDate(), transaction.getCreateDate());
+        assertEquals(capturedTransaction.getUpdateDate(), transaction.getUpdateDate());
+        assertEquals(capturedTransaction.getAccountTransactions(), transaction.getAccountTransactions());
+
 
     }
 }
