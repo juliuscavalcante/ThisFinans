93e0937ffc3f47d44e66f7ec53f2131a9d108504
Julius Cavalcante <julius.c@outlook.com.br>
2022-09-21 22:59:42 -0300

    004 - implementation of endpoints in AccountController save and findById

--- thisfinans/src/main/java/com/programeiros/thisfinans/controllers/AccountController.java
+++ thisfinans/src/main/java/com/programeiros/thisfinans/controllers/AccountController.java
@@ -2,18 +2,26 @@ package com.programeiros.thisfinans.controllers;
 
 import com.programeiros.thisfinans.exceptions.business.AccountNameRuleException;
 import com.programeiros.thisfinans.exceptions.business.AccountNameRuleWithParamsException;
+import com.programeiros.thisfinans.exceptions.mappers.AccountMapper;
 import com.programeiros.thisfinans.model.dto.AccountDTO;
+import com.programeiros.thisfinans.services.AccountService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.data.crossstore.ChangeSetPersister.NotFoundException;
+
+import javax.validation.Valid;
 
 
+@AllArgsConstructor
 @RestController
-@RequestMapping(path = "/")
+@RequestMapping(value = "/accounts")
 public class AccountController {
+
+    private final AccountService accountService;
+    private final AccountMapper mapper;
 
     @PostMapping(path = "validation")
     public ResponseEntity<AccountDTO> accountValidationEndpoint(@Validated @RequestBody AccountDTO accountDTO) {
@@ -29,5 +37,17 @@ public class AccountController {
             throw new AccountNameRuleException();
         }
         return ResponseEntity.ok("Success!");
+    }
+
+    @PostMapping
+    public ResponseEntity<Object> saveAccount(@RequestBody @Valid AccountDTO accountDTO) throws Exception {
+        AccountDTO newAccount = accountService.save(accountDTO);
+        return ResponseEntity.status(HttpStatus.CREATED).body(newAccount);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<AccountDTO> getOneAccount(@PathVariable(value = "id") Long id) throws NotFoundException {
+        AccountDTO accountDTO = accountService.findById(id);
+        return ResponseEntity.accepted().body(accountDTO);
     }
 }
